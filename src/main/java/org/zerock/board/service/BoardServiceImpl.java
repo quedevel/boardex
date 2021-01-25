a48aed66bdce2df9518b89be6a985d41d4729b9b
quedevel <kdh90817@naver.com>
2021-01-25 23:05:55 +0900

    N:1연관관계 CRUD 추가

---
+++ src/main/java/org/zerock/board/service/BoardServiceImpl.java
@@ -0,0 +1,57 @@
+package org.zerock.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Member;
+import org.zerock.board.repository.BoardRepository;
+import org.zerock.board.repository.ReplyRepository;
+
+import javax.transaction.Transactional;
+import java.util.function.Function;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    private final BoardRepository boardRepository;
+
+    private final ReplyRepository replyRepository;
+
+    @Override
+    public Long register(BoardDTO dto) {
+        log.info(dto);
+        Board board = dtoToEntity(dto);
+        boardRepository.save(board);
+        return board.getBno();
+    }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+        log.info(pageRequestDTO);
+        Function<Object[], BoardDTO> fn = entity -> entityToDTO((Board)entity[0],(Member)entity[1],(Long)entity[2]);
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public BoardDTO get(Long bno) {
+        Object result = boardRepository.getBoardByBno(bno);
+        Object[] arr = (Object[]) result;
+        return entityToDTO((Board) arr[0],(Member) arr[1],(Long) arr[2]);
+    }
+
+    @Transactional
+    @Override
+    public void removeWithReplies(Long bno) {
+        replyRepository.deleteByBno(bno);
+        boardRepository.deleteById(bno);
+    }
+}
